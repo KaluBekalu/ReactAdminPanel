87b22fca877556b61c96a56ce5f50b695438775e
KaluBekalu <kalubekalu1@gmail.com>
2021-07-17 14:37:00 +0300

    Fixed some bugs

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Topbar.css";
+import "./Tb.css";
 import { NotificationsNone, Language, Settings } from "@material-ui/icons";
 
 export default function Topbar() {
